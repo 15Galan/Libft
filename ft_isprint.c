23fea3d48168622646187a86a4c6be9b236506e9
Galán <antoniogalanG15@gmail.com>
2022-11-30 13:34:10 +0100

    Función 'ft_isprint' implementada

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/30 12:02:14 by antgalan          #+#    #+#             */
-/*   Updated: 2022/11/30 13:33:50 by antgalan         ###   ########.fr       */
+/*   Created: 2022/11/30 12:47:18 by antgalan          #+#    #+#             */
+/*   Updated: 2022/11/30 13:33:35 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-
-#endif
+/**
+ * @brief	Checks if the passed character is a printable character.
+ * 
+ * @param c 	The character to check.
+ * 
+ * @return	1 if the character is a printable character, 0 otherwise.
+ */
+int	ft_isprint(int c)
+{
+	return (32 <= c && c <= 126);
+}
