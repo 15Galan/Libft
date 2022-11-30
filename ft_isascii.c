c1ba4ff71d24a282199ea593a0b3fbdb72f1f8b2
Galán <antoniogalanG15@gmail.com>
2022-11-30 13:33:30 +0100

    Función 'ft_isascii' implementada

--- ft_isascii.c
+++ ft_isascii.c
@@ -1,24 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/30 12:02:14 by antgalan          #+#    #+#             */
-/*   Updated: 2022/11/30 13:32:43 by antgalan         ###   ########.fr       */
+/*   Created: 2022/11/30 12:24:41 by antgalan          #+#    #+#             */
+/*   Updated: 2022/11/30 13:32:35 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-
-#endif
+/**
+ * @brief	Checks if the passed character is an ASCII character.
+ * 
+ * @param c 	The character to check.
+ * 
+ * @return	1 if the character is an ASCII character, 0 otherwise.
+ */
+int	ft_isascii(int c)
+{
+	return (0 <= c && c <= 127);
+}
