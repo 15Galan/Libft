3e73e7e71c94c92b59298e0b8e0cb3a1de1bd076
Galán <antoniogalanG15@gmail.com>
2022-12-01 12:12:59 +0100

    Función 'ft_atoi' implementada

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 12:02:14 by antgalan          #+#    #+#             */
-/*   Updated: 2022/12/01 11:45:26 by antgalan         ###   ########.fr       */
+/*   Updated: 2022/12/01 11:55:46 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_isspace(int c);
+int		ft_atoi(const char *str);
 
 #endif
