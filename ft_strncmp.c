a4d7cafd51173ce22b02fca17cf5c1f8d2abbfdb
Galán <antoniogalanG15@gmail.com>
2022-12-09 17:24:26 +0100

    Función 'ft_strncmp' corregida
    Se tenían en cuenta los signos.

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 11:07:14 by antgalan          #+#    #+#             */
-/*   Updated: 2022/12/01 11:13:22 by antgalan         ###   ########.fr       */
+/*   Updated: 2022/12/09 17:23:51 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
  * @param s2	Second string to be compared.
  * @param n		Number of characters to be compared.
  * 
- * @return		The difference (number) between the first two different characters.
+ * @return	The difference (number) between the first two different characters.
  */
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
@@ -32,5 +32,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char) s1[i] - (unsigned char) s2[i]);
 }
