6ff80cb1a3a9560f4897a289433c2bb7cf880443
Galán <antoniogalanG15@gmail.com>
2022-12-09 21:03:23 +0100

    Función 'ft_putnbr_fd' corregida
    Se tienen en cuenta todos los casos y se gestiona el signo '-' correctamente.

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 18:20:57 by antgalan          #+#    #+#             */
-/*   Updated: 2022/12/02 18:21:49 by antgalan         ###   ########.fr       */
+/*   Updated: 2022/12/09 20:59:33 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,16 @@
 void	ft_putnbr_fd(int n, int fd)
 {
 	char	c;
+	long	num;
 
-	if (n < 0)
+	num = (long) n;
+	if (num < 0)
 	{
-		write(fd, "-", 1);
-		n *= -1;
+		ft_putchar_fd('-', fd);
+		num = -num;
 	}
-	if (n > 9)
-		ft_putnbr_fd(n / 10, fd);
-	c = (n % 10) + '0';
-	write(fd, &c, 1);
+	if (9 < num)
+		ft_putnbr_fd((int)(num / 10), fd);
+	c = (char) '0' + (num % 10);
+	ft_putchar_fd(c, fd);
 }
