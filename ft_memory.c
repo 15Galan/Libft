e49f8ca0bafae164e473b46ef643e14d89da00e0
Galán <antoniogalanG15@gmail.com>
2023-01-13 01:08:12 +0100

    Funciones sobre gestión de memoria agrupadas

---
+++ ft_memory.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memory.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 11:20:09 by antgalan          #+#    #+#             */
+/*   Updated: 2023/01/13 01:02:26 by antgalan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *str, int c, size_t n)
+{
+	unsigned char	*p;
+	unsigned int	i;
+
+	i = 0;
+	p = (unsigned char *) str;
+	while (i < n)
+	{
+		if (p[i] == (unsigned char) c)
+			return (p + i);
+		i++;
+	}
+	return (NULL);
+}
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*p1;
+	unsigned char	*p2;
+	unsigned int	i;
+
+	i = 0;
+	p1 = (unsigned char *) s1;
+	p2 = (unsigned char *) s2;
+	while (i < n)
+	{
+		if (p1[i] != p2[i])
+			return (p1[i] - p2[i]);
+		i++;
+	}
+	return (0);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (!dst && !src)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *) dst)[i] = ((unsigned char *) src)[i];
+		i++;
+	}
+	return (dst);
+}
+
+void	*ft_memmove(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (dst == src)
+		return (dst);
+	if (dst < src)
+	{
+		while (i < n)
+		{
+			((unsigned char *) dst)[i] = ((unsigned char *) src)[i];
+			i++;
+		}
+	}
+	else
+	{
+		while (0 < n)
+		{
+			((unsigned char *) dst)[n - 1] = ((unsigned char *) src)[n - 1];
+			n--;
+		}
+	}
+	return (dst);
+}
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+		((unsigned char *) s)[i++] = (unsigned char) c;
+	return (s);
+}
